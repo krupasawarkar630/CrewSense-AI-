21ce70f7a4a178c8a6b52adb24855effff586d7b
krupasawarkar630 <krupasawarkar630@gmail.com>
2026-09-18 12:07:13 +0530

    Fix structlog startup error

--- backend/app/core/logging.py
+++ backend/app/core/logging.py
@@ -15,7 +15,6 @@ def setup_logging() -> None:
 
     shared_processors: list[structlog.types.Processor] = [
         structlog.contextvars.merge_contextvars,
-        structlog.stdlib.add_logger_name,
         structlog.stdlib.add_log_level,
         structlog.processors.TimeStamper(fmt="iso"),
         structlog.processors.StackInfoRenderer(),
